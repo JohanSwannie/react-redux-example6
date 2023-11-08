1e144446fc74978281884879829ad401af4e3c81
Johan Swan <jswannieman@gmail.com>
2023-11-09 12:34:16 +1300

    Update index JS Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import store from "./store/store";
 import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 
