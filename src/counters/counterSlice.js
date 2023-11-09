2c61cfd413a26764068880fdc8b66a92568a4e64
Johan Swan <jswannieman@gmail.com>
2023-11-09 13:47:12 +1300

    Update counterSlice JS Component

--- src/counters/counterSlice.js
+++ src/counters/counterSlice.js
@@ -1,9 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import React from "react";
-
-const counterSlice = () => {
-  return <div>counterSlice</div>;
+const initialState = {
+  count: 0,
 };
 
-export default counterSlice;
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    multiply: (state) => {
+      state.count *= 5;
+    },
+    divide: (state) => {
+      state.count /= 2;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    addInput: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, multiply, divide, reset, addInput } =
+  counterSlice.actions;
+
+export default counterSlice.reducer;
